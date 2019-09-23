cb5921ac0f1a0b144fa550c45768146ba965eb2c
crare <Crare@users.noreply.github.com>
2019-09-23 20:33:13 +0300

    styling

--- src/components/ParticipantList.js
+++ src/components/ParticipantList.js
@@ -1,27 +1,81 @@
 import React from 'react';
 
+
+import { Edit, Delete, ArrowDownward, ArrowUpward } from '@material-ui/icons';
+
 export default class ParticipantList extends React.Component {
+
+  sortBy = "";
+  sortAscending = false;
 
   renderHeader() {
     return (
       <div className="list-header">
-        <div className="list-header-item-1">
-          Name
+        <div className="list-header-item flex-width-2" onClick={this.sortByColumn("name")}>
+          Name {this.renderSortIcon("name")}
         </div>
-        <div className="list-header-item-2">
-          E-mail address
+        <div className="list-header-item flex-width-3" onClick={this.sortByColumn("email")}>
+          E-mail address {this.renderSortIcon("email")}
         </div>
-        <div className="list-header-item-3">
-          Phone number
+        <div className="list-header-item flex-width-2" onClick={this.sortByColumn("phone")}>
+          Phone number {this.renderSortIcon("phone")}
         </div>
-        <div className="list-header-item-4">
-
-        </div>
-        <div className="list-header-item-5">
-
+        <div className="list-header-item flex-width-2">
         </div>
       </div>
     );
+  }
+
+  renderSortIcon(dataValue) {
+    if (this.sortBy === dataValue) {
+      if (this.sortAscending) {
+        return <ArrowUpward />
+      }
+      return <ArrowDownward />
+    }
+  }
+
+  sortByColumn(dataValue) {
+    if (this.lastSort === dataValue) {
+      this.sortAscending = !this.sortAscending;
+    }
+
+    const compare = (a, b, attr) => {
+      if (a[attr] < b[attr]) {
+        return 1;
+      }
+      if (a[attr] > b[attr]) {
+        return -1;
+      }
+      return 0;
+    }
+
+    const compareOpposite = (a, b, attr) => {
+      if (a[attr] < b[attr]) {
+        return -1;
+      }
+      if (a[attr] > b[attr]) {
+        return 1;
+      }
+      return 0;
+    }
+
+    if (this.sortAscending) {
+      this.props.participants.sort((a, b) => compare(a, b, dataValue));
+    } else {
+      this.props.participants.sort((a, b) => compareOpposite(a, b, dataValue));
+    }
+
+    this.lastSort = dataValue;
+  }
+
+  editRow(participantId) {
+    console.log("editRow, participantId:", participantId);
+
+  }
+
+  deleteParticipant(participantId) {
+    console.log("deleteParticipant, participantId:", participantId);
   }
 
   renderItems() {
@@ -30,12 +84,14 @@ export default class ParticipantList extends React.Component {
         {
           this.props.participants.map((p) => {
             return (
-              <div className="list-item">
-                <div className="list-item-property-1">{p.name}</div>
-                <div className="list-item-property-2">{p.email}</div>
-                <div className="list-item-property-3">{p.phone}</div>
-                <div className="list-item-property-4">edit</div>
-                <div className="list-item-property-5">delete</div>
+              <div key={p.id} className="list-item">
+                <div className="list-item-property flex-width-2">{p.name}</div>
+                <div className="list-item-property flex-width-3">{p.email}</div>
+                <div className="list-item-property flex-width-2">{p.phone}</div>
+                <div className="list-item-property flex-width-2 right" >
+                  <Edit className="edit" onClick={() => this.editRow(p.id)} />
+                  <Delete className="delete" onClick={() => this.deleteParticipant(p.id)} />
+                </div>
               </div>
             );
           })
--- src/components/ParticipantsForm.js
+++ src/components/ParticipantsForm.js
@@ -5,18 +5,18 @@ export default class ParticipantsForm extends React.Component {
   render() {
     return (
       <div className="participants-form">
-        <div className="participants-form-property-1">
+        <div className="form-item flex-width-2">
           <input name="name" placeholder="Full name" />
         </div>
-        <div className="participants-form-property-2">
-          <input name="email" placeholder="E-Mail address" />
+        <div className="form-item flex-width-3">
+          <input name="email" placeholder="E-mail address" />
         </div>
-        <div className="participants-form-property-3">
+        <div className="form-item flex-width-2">
           <input name="phone" placeholder="Phone number" />
         </div>
-        <div className="participants-form-property-4">
+        <div className="form-item flex-width-1">
         </div>
-        <div className="participants-form-property-5">
+        <div className="form-item flex-width-1 no-padding">
           <button>Add new</button>
         </div>
       </div>
