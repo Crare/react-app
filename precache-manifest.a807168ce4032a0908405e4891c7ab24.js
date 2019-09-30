be5811c0efbd0f7c81c19e8dc43163cfee2edfc7
crare <Crare@users.noreply.github.com>
2019-09-30 18:35:48 +0300

    Updates

---
+++ precache-manifest.a807168ce4032a0908405e4891c7ab24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "831c976a77aa36c8a45dc84f812773d6",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c04b9b476fca3c0e2a86",
+    "url": "/react-app/static/css/main.d3d0f44a.chunk.css"
+  },
+  {
+    "revision": "fe7d047eb0527fd9685c",
+    "url": "/react-app/static/js/2.2daeb2bc.chunk.js"
+  },
+  {
+    "revision": "c04b9b476fca3c0e2a86",
+    "url": "/react-app/static/js/main.e25908b6.chunk.js"
+  },
+  {
+    "revision": "298c5e9681f3b1ffff5a",
+    "url": "/react-app/static/js/runtime~main.36df2377.js"
+  }
+]);
